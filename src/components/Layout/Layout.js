c5b19ab4c79c67190dafd3efa3d033a202406282
kentOneR <blot.quentin@gmail.com>
2018-09-21 10:48:35 +0200

    add Toolbar in Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Auxi from 'Hoc/Auxi';
+import Toolbar from 'Components/Navigation/Toolbar/Toolbar';
 import './layout.scss';
 
 const layout = (props) => (
   <Auxi>
+    <Toolbar />
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className="content">
       {props.children}
