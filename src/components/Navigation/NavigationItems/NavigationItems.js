96a6565bc138042f9174ab37576479460e52edff
kentOneR <blot.quentin@gmail.com>
2018-09-27 18:03:53 +0200

    add NavLink to navigation

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './navigation_items.scss';
+import {NavLink} from 'react-router-dom';
 
 const navigationItems = (props) => (
   <nav>
     <ul className={`navigation-items ${props.show ? "active" : null}`}>
-      <li className="navigation-item"><a className="active" href="/">Burger Builder</a></li>
-      <li className="navigation-item"><a href="/">Checkout</a></li>
+      <li className="navigation-item"><NavLink to="/" exact>Burger Builder</NavLink></li>
+      <li className="navigation-item"><NavLink to="/orders">Orders</NavLink></li>
     </ul>
   </nav>
 );
