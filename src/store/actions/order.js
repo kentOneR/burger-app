86aa824fb2b6b5fec231f7645ee7920b5617744a
kentOneR <blot.quentin@gmail.com>
2018-10-10 12:45:35 +0200

    Orders: only fetch order by id

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -25,7 +25,7 @@ export const purchaseBurgerStart = () => {
 export const purchaseBurger = (orderData, token) => {
   return dispatch => {
     dispatch(purchaseBurgerStart());
-    axios.post('/orders.json' + token, orderData)
+    axios.post('/orders.json?auth=' + token, orderData)
     .then(res => {
       console.log(res.data);
       dispatch(burgerPurchaseSuccess(res.data.name, orderData));
@@ -62,10 +62,11 @@ export const fetchOrdersStart = () => {
   };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
   return dispatch => {
     dispatch(fetchOrdersStart());
-    axios.get('orders.json?auth=' + token)
+    const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+    axios.get('orders.json' + queryParams)
     .then(res => {
       let fetchedorders = [];
       for (let key in res.data) {
